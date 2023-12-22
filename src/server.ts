ce9c21ca66de0a3e7f3133b10bd08d9c43919dba
TheAndrey777 <andreyvasilchenko777@mail.ru>
2023-12-22 18:33:16 +0300

    База

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,10 @@
+import "reflect-metadata";
 import app from "./app";
+import { dataSource } from "../data/data.source";
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => { console.log(`Listening on port ${PORT}`) });
+dataSource.initialize()
+  .then(() => {
+      console.log("Successfully connected to the database");
+  }).catch((err) => { console.log(err)} );
