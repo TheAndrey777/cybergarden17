1b9687f75cea1712150149f32eb676fad74df762
Виталий <114582703+valavshonok@users.noreply.github.com>
2023-12-23 15:04:19 +0300

    Табличка на стр поиска

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import close from '/close-circle.svg';
+import museum from '/bank.svg';
+import ship from '/ship.svg';
+import theatre from '/theatre.svg';
+import gallery from '/gallery.svg';
+import sunfog from '/sun-fog.svg';
+import bag from '/bag.svg';
 
 export default function Search() {
   const arr = [{}, {}, {}, {}, {}];
@@ -18,6 +24,39 @@ export default function Search() {
       </div>
 
       <div className='text-[18px] text-white px-[20px]'>Категории</div>
+
+      <div className='grid grid-col-3 grid-rows-2 grid-flow-col justify-between px-[20px] pt-[20px]'>
+        <div className='h-[110px] w-[80px] relative'>
+          <div className='h-[80px] w-[80px] relative rounded-[10px] bg-[#181818] flex justify-center items-center'>
+            <img src={museum} alt='museum'></img>
+          </div>
+        </div>
+        <div className='h-[110px] w-[80px] relative'>
+          <div className='h-[80px] w-[80px] relative rounded-[10px] bg-[#181818] flex justify-center items-center'>
+            <img src={museum} alt='museum'></img>
+          </div>
+        </div>
+        <div className='h-[110px] w-[80px] relative'>
+          <div className='h-[80px] w-[80px] relative rounded-[10px] bg-[#181818] flex justify-center items-center'>
+            <img src={museum} alt='museum'></img>
+          </div>
+        </div>
+        <div className='h-[110px] w-[80px] relative'>
+          <div className='h-[80px] w-[80px] relative rounded-[10px] bg-[#181818] flex justify-center items-center'>
+            <img src={museum} alt='museum'></img>
+          </div>
+        </div>
+        <div className='h-[110px] w-[80px] relative'>
+          <div className='h-[80px] w-[80px] relative rounded-[10px] bg-[#181818] flex justify-center items-center'>
+            <img src={museum} alt='museum'></img>
+          </div>
+        </div>
+        <div className='h-[110px] w-[80px] relative'>
+          <div className='h-[80px] w-[80px] relative rounded-[10px] bg-[#181818] flex justify-center items-center'>
+            <img src={museum} alt='museum'></img>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
