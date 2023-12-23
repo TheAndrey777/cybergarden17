af8f7649b307ddfd0cf534149423b8fe018199d5
RomaTheKing <103886943+RomaTheKing@users.noreply.github.com>
2023-12-23 12:53:57 +0300

    add star display

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -1,22 +1,27 @@
-import React from 'react';
-import SelectStar from './StarRating';
+import React from "react";
+import SelectStar from "./StarRating";
 
 export default function Review() {
   return (
-    <div className='w-[100%] px-[20px]'>
-      <div className='h-[180px] w-[100%] bg-[#181818] rounded-[10px] box-border p-[10px] relative'>
-        <div className='text-[14px] overflow-hidden text-white'>Имя достопримечательности</div>
-        <div className='text-[12px] overflow-hidden text-[#4D4D4D] mb-[3px]'>Тип</div>
-        <SelectStar />
-        <div className=' bottom-[40px] text-[14px] h-[65px] w-[100%] text-ellipsis overflow-hidden text-white'>
-          Текстотзыва Текстотзы ва Текстотзыва Текстотз ыва Текстотзыва Текстотзыва Т екстотзыва
-          Текстотзыва перыеав ар праиапавраиарваиава пвиапви Текстотзыва Текстотзыва Т екстотзыва
-          Текстотзыва перыеав ар праиапавраиарваиава пвиапви
+    <div className="w-[100%] px-[20px]">
+      <div className="h-[180px] w-[100%] bg-[#181818] rounded-[10px] box-border p-[10px] relative">
+        <div className="text-[14px] overflow-hidden text-white">
+          Имя достопримечательности
         </div>
-        <div className='absolute right-[10px] bottom-[10px] text-[14px] text-[#4D4D4D]'>
+        <div className="text-[12px] overflow-hidden text-[#4D4D4D] mb-[3px]">
+          Тип
+        </div>
+        <SelectStar value={0} />
+        <div className=" bottom-[40px] text-[14px] h-[65px] w-[100%] text-ellipsis overflow-hidden text-white">
+          Текстотзыва Текстотзы ва Текстотзыва Текстотз ыва Текстотзыва
+          Текстотзыва Т екстотзыва Текстотзыва перыеав ар праиапавраиарваиава
+          пвиапви Текстотзыва Текстотзыва Т екстотзыва Текстотзыва перыеав ар
+          праиапавраиарваиава пвиапви
+        </div>
+        <div className="absolute right-[10px] bottom-[10px] text-[14px] text-[#4D4D4D]">
           12.08.2022
         </div>
-        <div className='absolute left-[10px] bottom-[10px] text-[14px] text-[#126A3A]'>
+        <div className="absolute left-[10px] bottom-[10px] text-[14px] text-[#126A3A]">
           Редактировать
         </div>
       </div>
--- client/src/components/StarRating.jsx
+++ client/src/components/StarRating.jsx
@@ -1,17 +1,43 @@
-import React from 'react';
-import starBold from '/bold-star.svg';
-import starEmpty from '/empty-star.svg';
-import starHalf from '/half-star.svg';
-// половинку
+import React from "react";
+import starBold from "/bold-star.svg";
+import starEmpty from "/empty-star.svg";
+import starHalf from "/half-star.svg";
 
-export default function SelectStar() {
+const STATES = [starEmpty, starHalf, starBold];
+
+function Star(props) {
+  console.log(props.state);
+  console.log(STATES[props.state]);
   return (
-    <div className='inline-flex'>
-      <img src={starBold} alt='star' className='h-[20px] w-[20px]'></img>
-      <img src={starBold} alt='star' className='h-[20px] w-[20px]'></img>
-      <img src={starBold} alt='star' className='h-[20px] w-[20px]'></img>
-      <img src={starBold} alt='star' className='h-[20px] w-[20px]'></img>
-      <img src={starBold} alt='star' className='h-[20px] w-[20px]'></img>
+    <img
+      src={STATES[props.state]}
+      alt="star"
+      className="h-[20px] w-[20px]"
+    ></img>
+  );
+}
+
+export default function SelectStar(props) {
+  const [value, setValue] = React.useState(props.value);
+  //console.log(value);
+
+  const stars = [];
+  for (let i = 0; i < 5; i++) {
+    //console.log(value - i);
+    stars.push(
+      new Star({
+        state: value - i >= 1 ? 2 : Math.max(Math.ceil(value - i), 0),
+      })
+    );
+  }
+
+  return (
+    <div className="inline-flex">
+      {stars[0]}
+      {stars[1]}
+      {stars[2]}
+      {stars[3]}
+      {stars[4]}
     </div>
   );
 }
