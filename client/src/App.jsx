08ece983c4e54ca400236ba2e181636e78018c4a
Виталий <114582703+valavshonok@users.noreply.github.com>
2023-12-23 23:56:21 +0300

    Роуты

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
-import SightBar from './components/SightBar.jsx';
+import { Route, Routes } from 'react-router-dom';
+import Auth from './pages/Auth.jsx';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
+import Search from './pages/Search.jsx';
+import Profile from './pages/Profile.jsx';
+import Favorites from './pages/Favorites.jsx';
 
 function App() {
   return (
     <div className='w-100% min-h-screen'>
-      <SightBar />
+      <Routes>
+        <Route path='/' element={<Auth />}></Route>
+        <Route path='auth' element={<Auth />}></Route>
+        <Route path='auth/login' element={<Login />}></Route>
+        <Route path='auth/register' element={<Register />}></Route>
+        <Route path='search' element={<Search />}></Route>
+        <Route path='profile' element={<Profile />}></Route>
+        <Route path='favourite' element={<Favorites />}></Route>
+      </Routes>
     </div>
   );
 }
