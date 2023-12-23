7b8929a590d89f9559c3fee67991e982b4cf5d63
RomaTheKing <103886943+RomaTheKing@users.noreply.github.com>
2023-12-23 23:35:26 +0300

    Add Interactive Stars to SelectStarRatting Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
-import SightBar from './components/SightBar.jsx';
+import SelectStar from "./components/SelectStarRating.jsx";
 
 function App() {
   return (
-    <div className='w-100% min-h-screen'>
-      <SightBar />
+    <div className="w-100% min-h-screen">
+      <SelectStar />
     </div>
   );
 }
--- client/src/components/SelectStarRating.jsx
+++ client/src/components/SelectStarRating.jsx
@@ -1,5 +1,11 @@
-import React from 'react';
+import React from "react";
+import StarRating from "./StarRating";
 
 export default function SelectStar() {
-  return <div>SelectStar</div>;
+  return (
+    <div>
+      SelectStar
+      <StarRating value={4.9} size={16} interactive={true} />
+    </div>
+  );
 }
