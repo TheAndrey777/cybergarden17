17e4ed948562753df41f72f2e17476e899310e86
Виталий <114582703+valavshonok@users.noreply.github.com>
2023-12-23 12:45:46 +0300

    Профиль, верстка

---
+++ client/src/components/PopularPlaces.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function PopularPlaces() {
+  return (
+    <div className='w-[100%] px-[20px]'>
+      <div className='h-[80px] w-[100%]'>PopularPlaces</div>
+    </div>
+  );
+}
--- client/src/components/ProfileHeader.jsx
+++ client/src/components/ProfileHeader.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import moon from '/moon.svg';
+import setting from '/setting.svg';
 
 export default function ProfileHeader() {
   return (
-    <div className='bg-red-400 h-[280px] w-[100%] relative'>
-      <div></div>
-      <div className='absolute flex items-center  justify-center bg-slate-300 h-[100%] w-[100%]'>
+    <div className=' h-[280px] w-[100%] relative'>
+      <div className='absolute inline-flex right-[20px] top-[20px]'>
+        <img src={moon} alt='theme' className='ml-[3px]'></img>
+        <img src={setting} alt='theme' className='ml-[3px]'></img>
+      </div>
+      <div className='absolute flex items-center  justify-center h-[100%] w-[100%]'>
         <div className=' justify-center text-center'>
           <div className='flex justify-center'>
             <div className='bg-[#4D4D4D] h-20 w-20 rounded-full'></div>
@@ -15,8 +19,8 @@ export default function ProfileHeader() {
         </div>
       </div>
 
-      <div></div>
-      <div>Отзывы</div>
+      <div className='absolute top-[240px] w-[100%] h-[1px] bg-[#4D4D4D]'></div>
+      <div className='absolute bottom-0 text-[18px] left-[20px] text-white'>Отзывы</div>
     </div>
   );
 }
