d1e80b85fe4a7821f00075108f8462ee498cdbf1
RomaTheKing <103886943+RomaTheKing@users.noreply.github.com>
2023-12-23 15:06:31 +0300

    Add interactive Stars

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
-import Search from './pages/Search.jsx';
+import Search from "./pages/Search.jsx";
+import Review from "./components/Review.jsx";
 
 function App() {
   return (
     <div>
-      <Search />
+      <Review />
     </div>
   );
 }
