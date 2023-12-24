5ce68208f9f5a1691fc6adf54baa84e7b4cdb099
RomaTheKing <103886943+RomaTheKing@users.noreply.github.com>
2023-12-24 08:14:35 +0300

    Add sorting

--- client/src/components/PopularPlaces.jsx
+++ client/src/components/PopularPlaces.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import mark from '/mark-favourite.svg';
-import arrow from '/arrow-right-circle.svg';
-import StarRating from './StarRating';
+import React from "react";
+import mark from "/mark-favourite.svg";
+import arrow from "/arrow-right-circle.svg";
+import StarRating from "./StarRating";
 
-export default function PopularPlaces() {
+export default function PopularPlaces(props) {
   return (
-    <div className='w-[100%] px-[20px] relative pt-[20px]'>
-      <div className='h-[90px] w-[100%] pr-[100px] pl-[15px] rounded-[10px] bg-[#181818] light:bg-[#EBEBEB] p-[10px]'>
-        <div className='text-white text-nowrap text-ellipsis overflow-hidden light:text-[#0D0D0D]'>
-          Имя достопримечательности
+    <div className="w-[100%] px-[20px] relative pt-[20px]">
+      <div className="h-[90px] w-[100%] pr-[100px] pl-[15px] rounded-[10px] bg-[#181818] light:bg-[#EBEBEB] p-[10px]">
+        <div className="text-white text-nowrap text-ellipsis overflow-hidden light:text-[#0D0D0D]">
+          {props.name}
         </div>
-        <div className='text-[12px] text-[#4D4D4D] mb-[3px]'>Тип</div>
-        <StarRating value={4.9} size={16} />
-        <div className='absolute h-[100%] w-8 right-[45px] pt-[20px] top-0 flex justify-center cursor-pointer'>
-          <img src={arrow} alt='arrow'></img>
+        <div className="text-[12px] text-[#4D4D4D] mb-[3px]">{props.tag}</div>
+        <StarRating value={props.value} size={16} />
+        <div className="absolute h-[100%] w-8 right-[45px] pt-[20px] top-0 flex justify-center cursor-pointer">
+          <img src={arrow} alt="arrow"></img>
         </div>
-        <div className='absolute h-[100%] w-8 right-[80px] top-[35px] light:[#0D0D0D>] cursor-pointer'>
-          <img src={mark} alt='mark'></img>
+        <div className="absolute h-[100%] w-8 right-[80px] top-[35px] light:[#0D0D0D>] cursor-pointer">
+          <img src={mark} alt="mark"></img>
         </div>
       </div>
     </div>
--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,13 +1,55 @@
-import React from 'react';
-import close from '/close-circle.svg';
-import museum from '/bank.svg';
-import ship from '/ship.svg';
-import theatre from '/theatre.svg';
-import gallery from '/gallery.svg';
-import sunfog from '/sun-fog.svg';
-import bag from '/bag.svg';
-import PopularPlaces from '../components/PopularPlaces';
-import CustomMap from '../components/CustomMap';
+import React from "react";
+import close from "/close-circle.svg";
+import museum from "/bank.svg";
+import ship from "/ship.svg";
+import theatre from "/theatre.svg";
+import gallery from "/gallery.svg";
+import sunfog from "/sun-fog.svg";
+import bag from "/bag.svg";
+import PopularPlaces from "../components/PopularPlaces";
+import CustomMap from "../components/CustomMap";
+
+let setPlaces1;
+
+const TEST_PLACES = [
+  { name: "Парк Петра", value: 3.7, desk: "Крутое место", tag: "Парки" },
+  { name: "Парк Гоголя", value: 2.2, desk: "Крутое место", tag: "Парки" },
+  { name: "Парк Гоголя", value: 2.2, desk: "Крутое место", tag: "Парки" },
+  { name: "Парк Гоголя", value: 2.2, desk: "Крутое место", tag: "Парки" },
+  { name: "Парк Гоголя", value: 2.2, desk: "Крутое место", tag: "Парки" },
+  { name: "Парк Гоголя", value: 2.2, desk: "Крутое место", tag: "Парки" },
+  {
+    name: "Памятник Петру",
+    value: 5,
+    desk: "Здесь можно побухать",
+    tag: "Памятники",
+  },
+];
+
+let load = (tag) => {
+  let select = [];
+  for (let i = 0; i < TEST_PLACES.length; i++) TEST_PLACES[i].key = i;
+  TEST_PLACES.forEach((el) => {
+    if (el.tag === tag) select.push(el);
+  });
+
+  setPlaces1(
+    <div>
+      {select.map((v, i) => {
+        console.log(v.value);
+        return (
+          <PopularPlaces
+            key={v.key}
+            name={v.name}
+            desk={v.desk}
+            value={v.value}
+            tag={v.tag}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 export default function Search() {
   const arr = [
@@ -23,80 +65,125 @@ export default function Search() {
     { 1: 1 },
     { 1: 1 },
     { 1: 1 },
-    { 1: 1 }
+    { 1: 1 },
   ];
 
+  const TAGS = ["Музеи", "Галереи", "Памятники", "Парки", "Театры", "Сувениры"];
+
   const menu = [
-    { title: 'Музеи', icon: museum, bg: 'bg-[#EA3597]', bordercolor: 'border-[#EA3597]' },
-    { title: 'Галереи', icon: gallery, bg: 'bg-[#35C9EA]', bordercolor: 'border-[#35C9EA]' },
-    { title: 'Памятники', icon: ship, bg: 'bg-[#EA4335]', bordercolor: 'border-[#EA4335]' },
-    { title: 'Парки', icon: sunfog, bg: 'bg-[#54A93F]', bordercolor: 'border-[#54A93F]' },
-    { title: 'Театры', icon: theatre, bg: 'bg-[#7A4FF4]', bordercolor: 'border-[#7A4FF4]' },
-    { title: 'Сувениры', icon: bag, bg: 'bg-[#EEA516]', bordercolor: 'border-[#EEA516]' }
+    {
+      title: "Музеи",
+      icon: museum,
+      bg: "bg-[#EA3597]",
+      bordercolor: "border-[#EA3597]",
+    },
+    {
+      title: "Галереи",
+      icon: gallery,
+      bg: "bg-[#EA4335]",
+      bordercolor: "border-[#EA4335]",
+    },
+    {
+      title: "Памятники",
+      icon: ship,
+      bg: "bg-[#7A4FF4]",
+      bordercolor: "border-[#7A4FF4]",
+    },
+    {
+      title: "Парки",
+      icon: sunfog,
+      bg: "bg-[#35C9EA]",
+      bordercolor: "border-[#35C9EA]",
+    },
+    {
+      title: "Театры",
+      icon: theatre,
+      bg: "bg-[#54A93F]",
+      bordercolor: "border-[#54A93F]",
+    },
+    {
+      title: "Сувениры",
+      icon: bag,
+      bg: "bg-[#EEA516]",
+      bordercolor: "border-[#EEA516]",
+    },
   ];
 
   const [active, setActive] = React.useState(0);
 
+  {
+    /* <div>
+  {popPlaces.map((v, i) => {
+    return <PopularPlaces key={i} />;
+  })}
+</div> */
+  }
+
+  const [places, setPlaces] = React.useState([]);
+  setPlaces1 = setPlaces;
   return (
     <>
-      <div className='z-10 absolute top-0 left-0 md:w-[400px] w-[100%] h-[100%] bg-black'>
+      <div className="z-10 absolute top-0 left-0 md:w-[400px] w-[100%] h-[100%] bg-black">
         <div
-          className='t-0 l-0 h-[100%] w-[100%] overflow-y-scroll overflow-x-hidden 
+          className="t-0 l-0 h-[100%] w-[100%] overflow-y-scroll overflow-x-hidden 
       [&::-webkit-scrollbar]:w-1
     [&::-webkit-scrollbar-track]:bg-[#000000]
     [&::-webkit-scrollbar-thumb]:bg-gray-300
     dark:[&::-webkit-scrollbar-track]:bg-[#000000]
     dark:[&::-webkit-scrollbar-thumb]:bg-slate-500
-    [&::-webkit-scrollbar-thumb]:rounded-full'
+    [&::-webkit-scrollbar-thumb]:rounded-full"
         >
-          <div className='relative w-[100%] px-[20px] h-[60px] pt-[10px]'>
-            <div className=' pr-[40px] h-[40px]'>
+          <div className="relative w-[100%] px-[20px] h-[60px] pt-[10px]">
+            <div className=" pr-[40px] h-[40px]">
               <input
-                type='text'
-                placeholder='Поиск достопримечательностей'
-                className='bg-[#181818] text-white light:[#EBEBEB] h-[40px] w-[100%] rounded-[10px] text-[15px] pl-[10px] text-ellipsis overflow-hidden'
+                type="text"
+                placeholder="Поиск достопримечательностей"
+                className="bg-[#181818] text-white light:[#EBEBEB] h-[40px] w-[100%] rounded-[10px] text-[15px] pl-[10px] text-ellipsis overflow-hidden"
               ></input>
             </div>
-            <div className='absolute h-[100%] w-[32px] light:[#0D0D0D] flex justify-center items-center bg-slate-500w-[100px] top-0 right-0 cursor-pointer'>
-              <img src={close} alt='close' className='absolute right-[20px]'></img>
+            <div className="absolute h-[100%] w-[32px] light:[#0D0D0D] flex justify-center items-center bg-slate-500w-[100px] top-0 right-0 cursor-pointer">
+              <img
+                src={close}
+                alt="close"
+                className="absolute right-[20px]"
+              ></img>
             </div>
           </div>
-          <div className='text-[18px] text-white light:[#0D0D0D] px-[20px]'>Категории</div>
-
-          <div className='grid grid-col-3 grid-rows-2 grid-flow-col justify-between px-[20px] pt-[20px]'>
+          <div className="text-[18px] text-white light:[#0D0D0D] px-[20px]">
+            Категории
+          </div>
+          <div className="grid grid-col-3 grid-rows-2 grid-flow-col justify-between px-[20px] pt-[20px]">
             {menu.map((v, i) => {
               return (
                 <div
                   key={i}
                   onClick={() => {
                     setActive(i);
+                    load(TAGS[i]);
                   }}
-                  className='h-[120px] w-[80px] relative light:bg-[#EBEBEB] cursor-pointer'
+                  className="h-[120px] w-[80px] relative light:bg-[#EBEBEB] cursor-pointer"
                 >
                   <div
                     className={`h-[80px] w-[80px] relative rounded-[10px] ${
-                      i === active ? v.bg : 'bg-[#181818]'
+                      i === active ? v.bg : "bg-[#181818]"
                     } border-2 ${
                       v.bordercolor
                     } flex justify-center items-center light:bg-[#0D0D0D]`}
                   >
                     <img src={v.icon} alt={v.title}></img>
                   </div>
-                  <div className='h-[30px] w-[80px] relative flex my-[5px] justify-center items-center text-white light:bg-[#0D0D0D]'>
+                  <div className="h-[30px] w-[80px] relative flex my-[5px] justify-center items-center text-white light:bg-[#0D0D0D]">
                     {v.title}
                   </div>
                 </div>
               );
             })}
           </div>
-
-          <div className='text-[18px] text-white light:[#0D0D0D] px-[20px] py-[7px]'>
+          <div className="text-[18px] text-white light:[#0D0D0D] px-[20px] py-[7px]">
             Популярные места
           </div>
-
-          {arr.map((v, i) => {
-            return <PopularPlaces key={i} />;
-          })}
+          places
+          <li>{places}</li>
         </div>
       </div>
       <CustomMap />
