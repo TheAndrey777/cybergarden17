33b86d4626cf6b16a0301f88fbc9f7bd04987d29
RomaTheKing <103886943+RomaTheKing@users.noreply.github.com>
2023-12-24 04:59:09 +0300

    adadad

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,32 +1,36 @@
-import React from 'react';
-import arrow from '/arrow-left.svg';
-import CustomMap from '../components/CustomMap';
-import { Link } from 'react-router-dom';
+import React from "react";
+import arrow from "/arrow-left.svg";
+import CustomMap from "../components/CustomMap";
+import { Link } from "react-router-dom";
 
 export default function Auth() {
   return (
     <>
-      <div className='z-10 absolute top-0 left-0 md:w-[400px] w-[100%] bg-black overflow-y-scroll overflow-x-hidden'>
-        <div className=' t-0 l-0 min-h-screen w-[100%] relative flex items-center justify-center'>
-          <img className='absolute top-[20px] left-[20px] text-white' src={arrow} alt='arrow'></img>
-          <div className='w-[100%]'>
-            <div className='text-[20px] text-white light:[#0D0D0D] text-center'>
-              {'Не совершен'}
+      <div className="z-10 absolute top-0 left-0 md:w-[400px] w-[100%] bg-black overflow-y-scroll overflow-x-hidden">
+        <div className=" t-0 l-0 min-h-screen w-[100%] relative flex items-center justify-center">
+          <img
+            className="absolute top-[20px] left-[20px] text-white"
+            src={arrow}
+            alt="arrow"
+          ></img>
+          <div className="w-[100%]">
+            <div className="text-[20px] text-white light:[#0D0D0D] text-center">
+              {"Не совершен"}
               <br />
-              {'вход в аккаунт'}
+              {"вход в аккаунт"}
             </div>
             <Link
-              to={'/auth/login'}
-              className='rounded-[10px] flex items-center justify-center mt-[20px] ml-[20px] mr-[20px] h-[50px] bg-[#126A3A]'
+              to={"/auth/login"}
+              className="rounded-[10px] flex items-center justify-center mt-[20px] ml-[20px] mr-[20px] h-[50px] bg-[#126A3A]"
             >
-              <div className='text-[18px] font-medium text-white'>{'Вход'}</div>
+              <div className="text-[18px] font-medium text-white">{"Вход"}</div>
             </Link>
             <Link
-              to={'/auth/register'}
-              className='rounded-[10px] flex items-center justify-center mt-[20px] ml-[20px] mr-[20px] h-[50px] bg-transparent box-border border-[1px] border-white light:border-[#0D0D0D]'
+              to={"/auth/register"}
+              className="rounded-[10px] flex items-center justify-center mt-[20px] ml-[20px] mr-[20px] h-[50px] bg-transparent box-border border-[1px] border-white light:border-[#0D0D0D]"
             >
-              <div className='text-[18px] font-medium text-white light:[#0D0D0D]'>
-                {'Регистрация'}
+              <div className="text-[18px] font-medium text-white light:[#0D0D0D]">
+                {"Регистрация"}
               </div>
             </Link>
           </div>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import arrow from '/arrow-left.svg';
-import google from '/google.svg';
-import vk from '/vk.svg';
-import CustomMap from '../components/CustomMap';
-import { Link } from 'react-router-dom';
+import React from "react";
+import arrow from "/arrow-left.svg";
+import google from "/google.svg";
+import vk from "/vk.svg";
+import CustomMap from "../components/CustomMap";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-const VALID = 'bg-[#181818] mb-[20px] pl-[15px] text-white h-[45px] w-[100%] rounded-[10px]';
-const INVALID = 'bg-[#501818] mb-[20px] pl-[15px] text-white h-[45px] w-[100%] rounded-[10px]';
+const VALID =
+  "bg-[#181818] mb-[20px] pl-[15px] text-white h-[45px] w-[100%] rounded-[10px]";
+const INVALID =
+  "bg-[#501818] mb-[20px] pl-[15px] text-white h-[45px] w-[100%] rounded-[10px]";
 
 const validateEmail = (email) => {
   return email.match(
@@ -22,9 +25,9 @@ export default function Register() {
 
   const click = () => {
     const request = {
-      username: document.getElementById('name').value,
-      email: document.getElementById('email').value,
-      password: document.getElementById('password').value
+      username: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      password: document.getElementById("password").value,
     };
 
     console.log(request);
@@ -35,31 +38,31 @@ export default function Register() {
     setPasswordRepeatColor(VALID);
 
     if (request.username.length == 0) {
-      console.log('Empty name');
+      console.log("Empty name");
       setNameColor(INVALID);
       return;
     }
 
     if (request.email.length == 0) {
-      console.log('Empty email');
+      console.log("Empty email");
       setEmailColor(INVALID);
       return;
     }
 
     if (!validateEmail(request.email)) {
-      console.log('Email is not valid');
+      console.log("Email is not valid");
       setEmailColor(INVALID);
       return;
     }
 
     if (request.password.length == 0) {
-      console.log('Empty password');
+      console.log("Empty password");
       setPasswordColor(INVALID);
       return;
     }
 
-    if (request.password != document.getElementById('password_repeat').value) {
-      console.log('Passwords are not the same');
+    if (request.password != document.getElementById("password_repeat").value) {
+      console.log("Passwords are not the same");
       setPasswordRepeatColor(INVALID);
       return;
     }
@@ -72,65 +75,82 @@ export default function Register() {
     )
       return;
 
-    Sender.sendPost({
-      adress: '10.131.56.212:8465/api/auth/register',
-      message: request
-      //f: (data) => localStorage.setItem("token", data.token),
-    });
+    axios
+      .post("http://10.131.56.212:8465/api/auth/register", request)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
 
-    console.log('Form is Valid');
+    console.log("Form is Valid");
   };
   return (
     <>
-      <div className='z-10 absolute top-0 left-0 md:w-[400px] w-[100%] bg-black'>
-        <div className=' t-0 l-0 h-dvh w-[100%] relative'>
-          <Link to={'/auth'} reloadDocument={false}>
+      <div className="z-10 absolute top-0 left-0 md:w-[400px] w-[100%] bg-black">
+        <div className=" t-0 l-0 h-dvh w-[100%] relative">
+          <Link to={"/auth"} reloadDocument={false}>
             <img
-              className='absolute top-[20px] left-[20px] text-white light:text-black'
+              className="absolute top-[20px] left-[20px] text-white light:text-black"
               src={arrow}
-              alt='arrow'
+              alt="arrow"
             ></img>
           </Link>
-          <div className='text-[20px] pt-[20%] text-white light:text-black text-center'>
-            {'Регистрация'}
+          <div className="text-[20px] pt-[20%] text-white light:text-black text-center">
+            {"Регистрация"}
           </div>
-          <div className='mx-[20px] mb-[10px] text-[#4D4D4D] light:text-[#181818]'>{'Имя'}</div>
-          <div className='px-[20px]'>
-            <input id='name' type='text' className={nameColor}></input>
+          <div className="mx-[20px] mb-[10px] text-[#4D4D4D] light:text-[#181818]">
+            {"Имя"}
           </div>
-          <div className='mx-[20px] mb-[10px] text-[#4D4D4D] light:text-[#181818]'>{'Почта'}</div>
-          <div className='px-[20px]'>
-            <input id='email' type='email' className={emailColor}></input>
+          <div className="px-[20px]">
+            <input id="name" type="text" className={nameColor}></input>
           </div>
-          <div className='mx-[20px] mb-[10px] text-[#4D4D4D] light:text-[#181818]'>{'Пароль'}</div>
-          <div className='px-[20px]'>
-            <input id='password' type='password' className={passwordColor}></input>
+          <div className="mx-[20px] mb-[10px] text-[#4D4D4D] light:text-[#181818]">
+            {"Почта"}
           </div>
-          <div className='mx-[20px] mb-[10px] text-[#4D4D4D] light:text-[#181818]'>
-            {'Повтор пароля'}
+          <div className="px-[20px]">
+            <input id="email" type="email" className={emailColor}></input>
           </div>
-          <div className='px-[20px]'>
-            <input id='password_repeat' type='password' className={passwordRepeatColor}></input>
+          <div className="mx-[20px] mb-[10px] text-[#4D4D4D] light:text-[#181818]">
+            {"Пароль"}
           </div>
-          <div className='absolute bottom-[10%] w-[100%]'>
-            <div className='rounded-[10px] flex items-center justify-center mt-[10%] ml-[20px] mr-[20px] h-[50px] bg-[#126A3A]'>
+          <div className="px-[20px]">
+            <input
+              id="password"
+              type="password"
+              className={passwordColor}
+            ></input>
+          </div>
+          <div className="mx-[20px] mb-[10px] text-[#4D4D4D] light:text-[#181818]">
+            {"Повтор пароля"}
+          </div>
+          <div className="px-[20px]">
+            <input
+              id="password_repeat"
+              type="password"
+              className={passwordRepeatColor}
+            ></input>
+          </div>
+          <div className="absolute bottom-[10%] w-[100%]">
+            <div className="rounded-[10px] flex items-center justify-center mt-[10%] ml-[20px] mr-[20px] h-[50px] bg-[#126A3A]">
               <button
                 onClick={click}
-                className='text-[18px] font-medium text-white light:text-black'
+                className="text-[18px] font-medium text-white light:text-black"
               >
-                {'Зарегистрировать'}
+                {"Зарегистрировать"}
               </button>
             </div>
-            <div className='relative rounded-[10px] flex items-center justify-center mt-[20px] ml-[20px] mr-[20px] h-[50px] bg-transparent box-border border-[1px] border-white light:border-black'>
-              <img className='absolute left-[15px]' src={google} alt='google'></img>
-              <div className='text-[18px] font-medium text-white light:text-black'>
-                {'Продолжить с google'}
+            <div className="relative rounded-[10px] flex items-center justify-center mt-[20px] ml-[20px] mr-[20px] h-[50px] bg-transparent box-border border-[1px] border-white light:border-black">
+              <img
+                className="absolute left-[15px]"
+                src={google}
+                alt="google"
+              ></img>
+              <div className="text-[18px] font-medium text-white light:text-black">
+                {"Продолжить с google"}
               </div>
             </div>
-            <div className='relative rounded-[10px] flex items-center justify-center mt-[20px] ml-[20px] mr-[20px] h-[50px] bg-transparent box-border border-[1px] border-white light:border-black'>
-              <img className='absolute left-[15px]' src={vk} alt='vk'></img>
-              <div className='text-[18px] font-medium text-white light:text-black'>
-                {'Продолжить с VK'}
+            <div className="relative rounded-[10px] flex items-center justify-center mt-[20px] ml-[20px] mr-[20px] h-[50px] bg-transparent box-border border-[1px] border-white light:border-black">
+              <img className="absolute left-[15px]" src={vk} alt="vk"></img>
+              <div className="text-[18px] font-medium text-white light:text-black">
+                {"Продолжить с VK"}
               </div>
             </div>
           </div>
