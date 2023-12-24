9af577dd1caff28ba2a41e9ec33088665ff7c5d3
RomaTheKing <103886943+RomaTheKing@users.noreply.github.com>
2023-12-24 12:44:27 +0300

    Add Favourite page

--- client/src/components/PopularPlaces.jsx
+++ client/src/components/PopularPlaces.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import favoriteMark from "/archive-tick.svg";
 import mark from "/mark-favourite.svg";
 import arrow from "/arrow-right-circle.svg";
 import StarRating from "./StarRating";
 
 export default function PopularPlaces(props) {
+  const [isFavoutite, setFavoutite] = React.useState(props.isFavoutite);
   return (
     <div className="w-[100%] px-[20px] relative pt-[20px]">
       <div className="h-[90px] w-[100%] pr-[100px] pl-[15px] rounded-[10px] bg-[#181818] light:bg-[#EBEBEB] p-[10px]">
@@ -16,7 +18,11 @@ export default function PopularPlaces(props) {
           <img src={arrow} alt="arrow"></img>
         </div>
         <div className="absolute h-[100%] w-8 right-[80px] top-[35px] light:[#0D0D0D>] cursor-pointer">
-          <img src={mark} alt="mark"></img>
+          <img
+            onClick={() => setFavoutite(!isFavoutite)}
+            src={isFavoutite ? favoriteMark : mark}
+            alt="mark"
+          ></img>
         </div>
       </div>
     </div>
