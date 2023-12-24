e98c98f022b55b9d3d03cb30577e9cff3b0ae530
Виталий <114582703+valavshonok@users.noreply.github.com>
2023-12-24 04:42:06 +0300

    Login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -56,36 +56,38 @@ export default function Login() {
   return (
     <>
       <div className='z-10 absolute top-0 left-0 md:w-[400px] w-[100%] bg-black'>
-        <div className=' t-0 l-0 h-dvh w-[100%] relative'>
-          <Link to={'/auth'} reloadDocument={false}>
-            <img
-              className='absolute top-[20px] left-[20px] text-white'
-              src={arrow}
-              alt='arrow'
-            ></img>
-          </Link>
-          <div className='text-[20px] pt-[20%] text-white text-center'>{'Авторизация'}</div>
-          <div className='mx-[20px] mb-[10px] text-[#4D4D4D]'>{'Почта'}</div>
-          <div className='px-[20px]'>
-            <input id='email' type='email' className={emailColor}></input>
-          </div>
-          <div className='mx-[20px] mb-[10px] text-[#4D4D4D]'>{'Пароль'}</div>
-          <div className='px-[20px]'>
-            <input id='password' type='password' className={passwordColor}></input>
-          </div>
-          <div className='absolute bottom-[10%] w-[100%]'>
-            <div className='rounded-[10px] flex items-center justify-center mt-[10%] ml-[20px] mr-[20px] h-[50px] bg-[#126A3A]'>
-              <button onClick={click} className='text-[18px] font-medium text-white'>
-                {'Войти'}
-              </button>
+        <div className=' t-0 l-0 h-dvh w-[100%] relative flex items-center justify-center'>
+          <div className='w-[100%]'>
+            <Link to={'/auth'} reloadDocument={false}>
+              <img
+                className='absolute top-[20px] left-[20px] text-white'
+                src={arrow}
+                alt='arrow'
+              ></img>
+            </Link>
+            <div className='text-[20px] text-white text-center'>{'Авторизация'}</div>
+            <div className='mx-[20px] mb-[10px] text-[#4D4D4D]'>{'Почта'}</div>
+            <div className='px-[20px]'>
+              <input id='email' type='email' className={emailColor}></input>
             </div>
-            <div className='relative rounded-[10px] flex items-center justify-center mt-[20px] ml-[20px] mr-[20px] h-[50px] bg-transparent box-border border-[1px] border-white'>
-              <img className='absolute left-[15px]' src={google} alt='google'></img>
-              <div className='text-[18px] font-medium text-white'>{'Продолжить с google'}</div>
+            <div className='mx-[20px] mb-[10px] text-[#4D4D4D]'>{'Пароль'}</div>
+            <div className='px-[20px]'>
+              <input id='password' type='password' className={passwordColor}></input>
             </div>
-            <div className='relative rounded-[10px] flex items-center justify-center mt-[20px] ml-[20px] mr-[20px] h-[50px] bg-transparent box-border border-[1px] border-white'>
-              <img className='absolute left-[15px]' src={vk} alt='vk'></img>
-              <div className='text-[18px] font-medium text-white'>{'Продолжить с VK'}</div>
+            <div className='bottom-[20px] w-[100%]'>
+              <div className='rounded-[10px] flex items-center justify-center mt-[20px] ml-[20px] mr-[20px] h-[50px] bg-[#126A3A]'>
+                <button onClick={click} className='text-[18px] font-medium text-white'>
+                  {'Войти'}
+                </button>
+              </div>
+              <div className='relative rounded-[10px] flex items-center justify-center mt-[20px] ml-[20px] mr-[20px] h-[50px] bg-transparent box-border border-[1px] border-white'>
+                <img className='absolute left-[15px]' src={google} alt='google'></img>
+                <div className='text-[18px] font-medium text-white'>{'Продолжить с google'}</div>
+              </div>
+              <div className='relative rounded-[10px] flex items-center justify-center mt-[20px] ml-[20px] mr-[20px] h-[50px] bg-transparent box-border border-[1px] border-white'>
+                <img className='absolute left-[15px]' src={vk} alt='vk'></img>
+                <div className='text-[18px] font-medium text-white'>{'Продолжить с VK'}</div>
+              </div>
             </div>
           </div>
         </div>
